02c60ea88e626e071cc0d889579584372f4326db
Glitch (glitch-hello-website) <none>
2024-03-21 04:34:42 +0000

    💷🏦 Checkpoint ./script.js:65462025/355 ./style.css:65462025/24

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const slideContainer = 
+const IMG_WIDTH = 400
+const slideContainer = document.querySelector('.slides')
 const slideImages = [...document.querySelectorAll('.slide')]
+
+slideContainer.style.width = `${slideImages.length * IMG_WIDTH}px`
+
+let index = 0
+
+function next(){
+  if (index < slideImages.length - 1){
+    index++
+  }
+  else index = 0
+  slideContainer.style.transform = `translateX(-${IMG_WIDTH*index}px)`
+}
